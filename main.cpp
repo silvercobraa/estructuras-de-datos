a2a870584d070a1e7745f8d54371187ca72cd88c
silvercobraa <silver.cobra1n@gmail.com>
2016-06-30 12:19:50 -0400

    Nada importante

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include "Node.hpp"
 #include "SingleLinkedList.cpp"
+#include "BiNode.hpp"
 
 int main(int argc, char const *argv[])
 {
 	Node<int>* n = new Node<int>(4);
 	SingleLinkedList<int>* sll = new SingleLinkedList<int>();
-	// sll->remove_first();
+	sll->add_last(3);
+	sll->add_first(2);
+	sll->remove_first();
+	BiNode<int>* bn = new BiNode<int>(4);
 	return 0;
 }
